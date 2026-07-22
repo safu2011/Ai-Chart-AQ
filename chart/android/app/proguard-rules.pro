df6e94cefa471737a7e6ba5b62395ecef0f94d3e
Saif Qureshi <48083045+safu2011@users.noreply.github.com>
2026-07-22 22:39:47 +0500

    target sdk updated and billin lib updated

---
+++ chart/android/app/proguard-rules.pro
@@ -0,0 +1,3 @@
+# R8 rules to fix missing classes in billingclient
+-dontwarn com.android.billingclient.api.QueryPurchaseHistoryParams$Builder
+-dontwarn com.android.billingclient.api.QueryPurchaseHistoryParams
--- chart/android/build.gradle.kts
+++ chart/android/build.gradle.kts
@@ -36,7 +36,9 @@ subprojects {
 }
 
 subprojects {
-    project.evaluationDependsOn(":app")
+    if (project.name != "app") {
+        project.evaluationDependsOn(":app")
+    }
 }
 
 tasks.register<Delete>("clean") {
