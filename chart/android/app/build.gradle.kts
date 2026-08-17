927a10f89dc1edd494b9f488273c30cfae1ed0c2
Saif Qureshi <48083045+safu2011@users.noreply.github.com>
2026-08-17 23:54:33 +0500

    ads updated

--- chart/android/app/build.gradle.kts
+++ chart/android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.aq.aichartanalyzer.cryptosignals"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
     }
 
     buildTypes {
